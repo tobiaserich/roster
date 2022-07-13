09d4cae0962c5f3f0df2ef286578ce6ca9a36abe
tobiaserich <tobiaserich@hotmail.de>
2022-07-13 10:24:11 +0200

    Add Globalstyles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import GlobalStyles from "./GlobalStyles";
 import Main from "./pages/Main";
 
-function App() {
-  return <Main />;
-}
+const App = () => {
+  return (
+    <>
+      <GlobalStyles />
+      <Main />
+    </>
+  );
+};
 
 export default App;
