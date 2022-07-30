ce135ffe3198dc3f594e85dcc1156695e9a9521b
tobiaserich <tobiaserich@hotmail.de>
2022-07-30 14:40:35 +0200

    Add popupmenu for temporary file upload. Also add function for handling data to check and add to db

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -3,35 +3,80 @@ import Header from "../components/Header";
 import getRoster from "../helper/getRoster";
 import Rosters from "../components/Rosters";
 import { PageContext } from "../App";
+import PopUpMenu from "../components/PopUpMenu";
+import { db } from "../helper/db";
+import { useLiveQuery } from "dexie-react-hooks";
 
 const Main = () => {
   const context = React.useContext(PageContext);
+  const [settingsVisible, setSettingsVisible] = React.useState(false);
+  const [data, setData] = React.useState("");
   const changeCurrentCluster = (clusterName) => {
     context.setCurrentCluster(clusterName);
   };
 
+  const changeSettingsVisibility = (e, input) => {
+    if (input === true) {
+      e.currentTarget.parentElement.parentElement.scroll(0, 0);
+      setSettingsVisible(input);
+    }
+
+    if (!input) {
+      if (e.target === e.currentTarget) {
+        setSettingsVisible(false);
+      }
+    }
+  };
+
   React.useEffect(() => {
     const allPages = async () => {
-      const result = await getRoster();
+      const indexTable = await db.files.toArray();
+      const allFiles = await db.data.toArray();
+      if (data === "") {
+        return;
+      }
+      const result = await getRoster(data);
       context.setAllData(result);
+      console.log(indexTable);
+      console.log(allFiles);
+      const searchInIndex = indexTable.find(
+        (entry) => entry.month === result.month && entry.year === result.year
+      );
+      if (searchInIndex === undefined) {
+        db.files.add({ month: result.month, year: result.year });
+        db.data.add({ month: result.month, year: result.year, data: result });
+      }
+
       if (context.currentCluster === "") {
         context.setCurrentCluster(result.cluster[0]);
       }
     };
 
-    context.allData || allPages();
-  }, []);
+    allPages();
+  }, [data]);
 
   const employeesInCurrentCluster = () =>
-    context.allData?.employee.filter(
+    context.allData?.employee?.filter(
       (employee) => employee.cluster === context.currentCluster
     );
 
-  return context.allData ? (
+  return (
     <>
+      {settingsVisible ? (
+        <PopUpMenu
+          closeSettings={(e) => changeSettingsVisibility(e, false)}
+          setFile={setData}
+        />
+      ) : (
+        ""
+      )}
       <Header
-        cluster={[context.currentCluster, ...context.allData.cluster]}
+        cluster={[
+          context?.currentCluster,
+          ...(context?.allData?.cluster ?? ""),
+        ]}
         changeCluster={changeCurrentCluster}
+        openSettings={(e) => changeSettingsVisibility(e, !settingsVisible)}
       ></Header>
 
       <Rosters
@@ -40,10 +85,9 @@ const Main = () => {
           year: context.allData.year,
           month: context.allData.month,
         }}
+        disableScrollX={settingsVisible}
       />
     </>
-  ) : (
-    <></>
   );
 };
 
